8910c2fcd5578e951df2990b691ada05c4b1eeb1
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-09-10 11:48:37 +0000

    [Add] JS5章確認問題

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/*global $*/
 $(function(){
-  $('.box1').hide();
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp(3000);
+  });
 });

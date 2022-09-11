dd4ee86404a3f2f0391669feba0815a6f994e047
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-09-11 03:08:13 +0000

    [Update] JS7章：children

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*global $*/
 
 $(function(){
-  $('.box1').on('click', function(){
-    $(this).slideUp();
+  $('button').on('click', function(){
+    $('ul').children().css('color', 'red');
   });
 });

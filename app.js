15160916a569225d8f28bf13a75c37683d6dd3fc
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-09-11 03:00:08 +0000

    [Update] JS7章：this

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 /*global $*/
+
 $(function(){
-    $('.box1').on('click', function(){
-        $('.box1').addClass('box1-ext');
-    });
-    $('.box1').mouseout(function(){
-        $('.box1').removeClass('box1-ext');
-    });
+  $('.box1').on('click', function(){
+    $(this).slideUp();
+  });
 });
